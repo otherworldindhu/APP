3787a2f2175e3a7f202077be595ea54ed6321669
otherworldindhu <80580975+otherworldindhu@users.noreply.github.com>
2022-06-30 21:01:58 +0530

    Update 1st.py

--- project/1st.py
+++ project/1st.py
@@ -1,11 +1,8 @@
 import tkinter as tk   
-root=tk.Tk()      #The second line created an instance of the tkinter.Tk class. This created what is called the "root" window 
+root=tk.Tk()      
 
 root.title("Journaling")
-#label=Label(root,text="I am a label widget") #added a new instance named label for the Label widget. The first parameter defined root as its parent or container. The second parameter configured its text option as I am a label widget.
-#button=Button(root,text="I am a button") #we defined an instance of a Button widget. This is also bound to the root window as its parent.
-#label.pack() #We used the pack() method, which is essentially required to position the label and button widgets within the window. 
-#button.pack()
+
 indhu=tk.Frame(root,relief="raised",bd=12)
 indhu.pack(fill=tk.X)
 snega=tk.Menubutton(indhu,text="All entries",)
@@ -106,11 +103,4 @@ bottom_pane_text = tk.Text(my_paned_window_2, height=3, width=3)
 my_paned_window_2.add(bottom_pane_text)
 
 root.mainloop()  
-"""The  line executed the mainloop (that is, the event loop) method of the root object. The mainloop method is what keeps the root window visible. 
-If you remove the third line, the window created in line 2 will disappear 
-immediately as soon as the script stops running. This will happen so fast that 
-you will not even see the window appearing on your screen. Keeping the 
-mainloop method running also lets you keep the program running until you 
-press the close button, which exits the main loop.
-• Tkinter also exposed the mainloop method as tkinter.mainloop(). So, you 
-can even call mainloop() directly instead of calling root.mainloop()."""
+
